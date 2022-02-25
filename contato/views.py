ab2405bb8265be45857504830c2bf7ffe1ee01de
= <=>
2022-02-25 16:29:06 -0400

    Campo de busco no listview

--- contato/views.py
+++ contato/views.py
@@ -1,7 +1,9 @@
 from ast import Delete
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, TemplateView, UpdateView, DeleteView
+from django.views.generic import (CreateView, DeleteView, ListView,
+                                  TemplateView, UpdateView)
 
 from contato.forms import ContatoForm
 from contato.models import Contato
@@ -15,8 +17,24 @@ class ContatoView(TemplateView):
 
 class ContatoListView(ListView):
     model = Contato
-    queryset = Contato.objects.all().order_by('nome')
+    # queryset = Contato.objects.all().order_by('nome')
     template_name = 'contato/contato_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro_nome = self.request.GET.get('nome') or None
+        filtro_tipo_contato = self.request.GET.get('tipo_contato') or None
+
+        if filtro_nome:
+            queryset = queryset.filter(nome__contains=filtro_nome)
+        if filtro_tipo_contato == 'PF':
+            queryset = queryset.filter(tipo_contato='PF')
+        elif filtro_tipo_contato == 'PJ':
+            queryset = queryset.filter(tipo_contato='PJ')
+        else:
+            queryset = queryset.filter()
+
+        return queryset
 
 
 class ContatoCreateView(CreateView):
@@ -24,17 +42,17 @@ class ContatoCreateView(CreateView):
     field = '__all__'
     template_name = 'contato/contato_create.html'
     form_class = ContatoForm
-    success_url = reverse_lazy('contato-home')
+    success_url = reverse_lazy('contato-lista')
+
 
 class ContatoUpdateView(UpdateView):
     model = Contato
     field = '__all__'
     template_name = 'contato/contato_create.html'
     form_class = ContatoForm
-    success_url = reverse_lazy('contato-home')
+    success_url = reverse_lazy('contato-lista')
 
 
 class ContatoDeleteView(DeleteView):
     model = Contato
-    success_url = reverse_lazy('contato-home')
-    
+    success_url = reverse_lazy('contato-lista')
